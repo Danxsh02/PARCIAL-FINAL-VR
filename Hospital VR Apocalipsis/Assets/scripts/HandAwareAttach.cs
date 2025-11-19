e67525b53fb4e785a7455d5dec7d1ccfa8696a9a
YenMur <yennifer.murillo@uao.edu.co>
2025-11-19 17:36:48 -0500

    AttachPointCompleto-ParteLogicaFinal

---
+++ Hospital VR Apocalipsis/Assets/scripts/HandAwareAttach.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+[RequireComponent(typeof(XRGrabInteractable))]
+public class HandAwareAttach : MonoBehaviour
+{
+    [Header("Attach Points")]
+    public Transform attachLeft;
+    public Transform attachRight;
+
+    [Header("Controladores de mano")]
+    public Transform leftHandController;
+    public Transform rightHandController;
+
+    private XRGrabInteractable grab;
+
+    void Awake()
+    {
+        grab = GetComponent<XRGrabInteractable>();
+    }
+
+    void Start()
+    {
+        // Fix del primer agarre: asignar un attach inicial
+        if (attachRight != null)
+            grab.attachTransform = attachRight;
+    }
+
+    void Update()
+    {
+        if (grab == null) return;
+
+        // Solo cambiar attach cuando NO está siendo agarrado
+        if (!grab.isSelected && leftHandController != null && rightHandController != null)
+        {
+            float distLeft = Vector3.Distance(transform.position, leftHandController.position);
+            float distRight = Vector3.Distance(transform.position, rightHandController.position);
+
+            // Elige la mano más cercana
+            if (distRight < distLeft && attachRight != null)
+            {
+                if (grab.attachTransform != attachRight)
+                    grab.attachTransform = attachRight;
+            }
+            else if (attachLeft != null)
+            {
+                if (grab.attachTransform != attachLeft)
+                    grab.attachTransform = attachLeft;
+            }
+        }
+    }
+}
